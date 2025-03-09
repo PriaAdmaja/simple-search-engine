bb9ab7fdc3035c291f838b39a7d3f9cab0ff7fd3
Pria Admaja <priaadmaja@gmail.com>
2025-03-09 10:34:30 +0700

    Refactoring filteringData

--- src/utils/filteringData.js
+++ src/utils/filteringData.js
@@ -1,3 +1,7 @@
+import { keywordsScore } from "./keywordsScore.js";
+import { distance } from "./levenhsteinDistance.js";
+import { sortingDataScore } from "./sortingDataScore.js";
+
 // data example =
 // [{
 //     "id": "110108",
@@ -17,38 +21,30 @@
 //     "updated_at": "2025-02-09T12:21:04.000Z"
 //     }]
 
-import { distance } from "./levenhsteinDistance.js";
-
 export function filteringData(data, search) {
   return (
     data
       // set distance
-      .map((datum) => ({
-        datum,
-        district: distance(datum.district_name, search),
-        city: distance(datum.city_name, search),
-        province: distance(datum.province_name, search),
-      }))
+      .map((datum) => {
+        return {
+          datum,
+          district: distance(datum.district_name, search),
+          city: distance(datum.city_name, search),
+          province: distance(datum.province_name, search),
+          keywords: keywordsScore(datum, search),
+        };
+      })
 
       // filtring distance is not more than 2
       .filter(
-        ({ district, city, province }) =>
-          Math.min(district, city, province) <= 2
+        ({ district, city, province, keywords }) =>
+          Math.min(district, city, province, keywords) <= 2
       )
 
       // sort
-      .sort((a, b) => {
-        const minA = Math.min(a.district, a.city, a.province);
-        const minB = Math.min(b.district, b.city, b.province);
-        // check distance value first
-        if (minA !== minB) return minA - minB;
-        // then sort by priority of distric, city, or province
-        if (a.district !== b.district) return a.district - b.district;
-        if (a.city !== b.city) return a.city - b.city;
-        return a.province - b.province;
-      })
+      .sort(sortingDataScore)
 
       // returning original data
-      .map(({ datum }) => datum)
+      // .map(({ datum }) => datum)
   );
 }
